cd98cde57de4811cf9581016dbdacb14b0e4b44d
Phalakorn223 <phalakorn061@gmail.com>
2021-11-19 10:37:15 +0700

    =Update v1

---
+++ php301.php
@@ -0,0 +1,11 @@
+<?php
+    $x = 10;
+    $y = 5;
+    echo "<h1'>PHP Arithmetic Operators</h1>";
+    echo"x = $x and y = $y","<hr color ='red siz='4' />"
+    echo "x + y = ", $x + $y,"<br />";
+    echo "x - y = ", $x - $y,"<br />";
+    echo "x * y = ", $x * $y,"<br />";
+    echo "x / y = ", $x / $y,"<br />";
+    echo "x % y = ", $x % $y,"<br />";
+    echo "x^y = ", $x**$y,"<br />";
